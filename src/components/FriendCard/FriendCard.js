4bca9eb014377ad24bfa14c07cdc28e1e70da5aa
kevlarpixels <kevingaguilar1@gmail.com>
2018-12-26 23:05:49 -0800

    update

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -2,9 +2,12 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-    <div className="card" value={props.id} onClick={() => props.handleClick(props.id)}>
+    <div className="card" 
+    value={props.id}
+    onClick={() => props.handleClick(props.id)}>
+        
         <div className="img-container">
-        <img alt={props.name} src={props.images} />
+        <img alt={props.name} src={props.image} />
         </div>
     </div>
 );
