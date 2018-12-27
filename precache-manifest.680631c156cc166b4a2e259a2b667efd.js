a46d225b18c6c0cd927481582379c7e94cf02049
kevlarpixels <kevingaguilar1@gmail.com>
2018-12-27 00:01:55 -0800

    Updates

---
+++ precache-manifest.680631c156cc166b4a2e259a2b667efd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f92b3e7b15186eeba144",
+    "url": "/ClickyGameReact/static/js/runtime~main.f92b3e7b.js"
+  },
+  {
+    "revision": "6c959f3b3ed417b3f22c",
+    "url": "/ClickyGameReact/static/js/main.6c959f3b.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/ClickyGameReact/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "6c959f3b3ed417b3f22c",
+    "url": "/ClickyGameReact/static/css/main.f5369543.chunk.css"
+  },
+  {
+    "revision": "8ba14eab80bb675450059e25e0c3a5fe",
+    "url": "/ClickyGameReact/index.html"
+  }
+];
