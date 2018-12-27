7b11d80fa17b5c97cc0229c5494bd40b42949e9e
kevlarpixels <kevingaguilar1@gmail.com>
2018-12-26 22:46:42 -0800

    it works except for the images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
@@ -7,13 +7,12 @@ import Container from "./Container";
 import Row from "./Row";
 import Column from "./Column";
 import friends from "./friends.json";
-import './App.css';
-
+import "./App.css";
 
 function shuffleFriends(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[1]];
+    [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 };
@@ -26,85 +25,83 @@ class App extends Component {
     currentScore: 0,
     topScore: 0,
     rightWrong: "",
-    clicked: []
+    clicked: [],
   };
 
 
-    handleClick = id => {
-      if (this.state.clicked.indexOf(id) === -1) {
-        this.handleIncrement();
-        this.setState({ clicked: this.state.clicked.concat(id) });
-      } else {
-        this.handleReset();
-      }
-    };
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
 
-    handleIncrement = () => {
-      const newScore = this.state.currentScore + 1;
-      this.setState({
-        currentScore: newScore,
-        rightWrong: ""
-      });
-      if (newScore >= this.state.topScore) {
-        this.setState({ topScore: newScore});
-      } else if (newScore === 12) {
-        this.setState.setState({ rightWrong: "You win!" });
-      }
-      this.handleShuffle();
-    };
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    } else if (newScore === 12) {
+      this.setState.setState({ rightWrong: "You win!" });
+    }
+    this.handleShuffle();
+  };
 
-    handleReset = () => {
-      this.setState({
-        currentScore: 0,
-        topScore: this.state.topScore,
-        rightWrong: "That's what she said",
-        clicked: []
-      });
-      this.handleShuffle();
-    };
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "That's what she said",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
 
-    handleShuffle = () => {
-      let shuffleFriends = shuffleFriends(friends);
-      this.setState({ friends: shuffleFriends });
-    };
+  handleShuffle = () => {
+    let shuffledFriends = shuffleFriends(friends);
+    this.setState({ friends: shuffledFriends });
+  };
 
-    render() {
-      return (
-        <Wrapper>
-          <Nav
-          title="The Office Memory Game"
+  render() {
+    return (
+      <Wrapper>
+        <Nav
+          title="The Office"
           score={this.state.currentScore}
           topScore={this.state.topScore}
           rightWrong={this.state.rightWrong}
-          />
+        />
 
-          <Title>
-           “Through concentration, I can raise and lower my cholesterol at will.”
-           Click on each character, but do not hit any duplicates, or you will not be able to do what Dwight is able to.
+        <Title>
+          “Through concentration, I can raise and lower my cholesterol at will.”<br />
+          Click on each character, but do not hit any duplicates, or you will not be able to do what Dwight is able to.
           </Title>
 
-          <Container>
-            <Row>
-              {this.state.friends.map(friend => (
-                <Column size="md-3 sm.6">
+        <Container>
+          <Row>
+            {this.state.friends.map(friend => (
+              <Column size="md-3 sm-6">
                 <FriendCard
-                key={friend.id}
-                handleClick={this.handleClick}
-                handleIncrement={this.handleIncrement}
-                handleReset={this.handleReset}
-                handleShuffle={this.handleShuffle}
-                id={friend.id}
-                image={friend.image}
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
                 />
-                </Column>
-              ))}
-            </Row>
-          </Container>
-        </Wrapper>
-      )
-    }
-  
-
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -3,7 +3,7 @@ import "./FriendCard.css";
 
 const FriendCard = props => (
     <div className="card" value={props.id} onClick={() => props.handleClick(props.id)}>
-        <div ClassName="img-container">
+        <div className="img-container">
         <img alt={props.name} src={props.images} />
         </div>
     </div>
